1d2b21b055260ed669f91aff2f2aa449bc24c092
devi <devidhatla>
2024-11-28 14:00:12 +0530

    foreach

--- day-10-count-foreach/foreach/main.tf
+++ day-10-count-foreach/foreach/main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "name" {
     ami = var.ami
     instance_type = var.type
     key_name = var.key
-    count = length(var.namee)
+    for_each = toset(var.namee)
     tags = {
-      Name = var.namee[count.index]
+      Name = each.value
     }
 }
---
+++ day-10-count-foreach/foreach/variable.tf
@@ -0,0 +1,22 @@
+variable "ami" {
+    type = string
+    default = " "
+  
+}
+
+variable "type" {
+    type = string
+    default = " "
+  
+}
+variable "key" {
+    type = string
+    default = " "
+  
+}
+
+variable "namee" {
+    type = list(string)
+    default = [ "devi","sai"]
+  
+}
